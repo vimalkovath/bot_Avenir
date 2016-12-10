766fecdd46936f2cac1341aae145dd19ab8a6078
Gokul <Gokul>
2016-12-11 02:04:12 +0530

    bot webhook

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ app.listen(port, function () {
   console.log('Listening on port ' + port);
 });
 
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
 
 app.post('/hello', function (req, res, next) {
   var userName = req.body.user_name;
